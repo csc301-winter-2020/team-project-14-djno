441a005820ac550e2cb985a091f95452bf03b1e0
Truman Hung <t.hung@mail.utoronto.ca>
2020-02-14 15:23:16 -0400

    Create-react-app

---
+++ deliverable-2/helpouts/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    /* If not login, show Login screen, possibly with interactive setup*/
+    // If login, show main page
+    <div className="App">
+      Content
+
+    </div>
+  );
+}
+
+export default App;
