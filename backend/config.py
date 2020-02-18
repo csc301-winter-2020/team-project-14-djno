10a31773ac95df820618ba6e1f533c6cddb7872c
aionWilliam <zhaiwilliam@gmail.com>
2020-02-18 16:10:16 -0500

    added connection code to database, created database model for user classes

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+# database constants
+DATABASE_NAME = 'admin'
+HOST_IP = '159.203.53.245'
+PORT = 27017
+USERNAME = 'admin'
+PASSWORD = 'admin123'
+AUTHENTICATION_SOURCE = 'admin'
---
+++ backend/model/UserModel.py
@@ -0,0 +1,53 @@
+from mongoengine import *
+from datetime import datetime
+from backend.config import *
+
+res = connect(DATABASE_NAME, host=HOST_IP, port=PORT, username=USERNAME, password=PASSWORD,
+              authentication_source=AUTHENTICATION_SOURCE)
+print(res)
+
+# todo: add default values and data length restrictions
+
+
+class Preferences(EmbeddedDocument):
+    education_navigation = BooleanField()
+    education_support = BooleanField()
+    employment_navigation = BooleanField()
+    employment_support = BooleanField()
+    health_care_navigation = BooleanField()
+    health_care_support = BooleanField()
+    local_navigation = BooleanField()
+    local_support = BooleanField()
+    well_being_leisure = BooleanField()
+    pick_up_and_delivery = BooleanField()
+    pick_up_and_drop_off = BooleanField()
+    homemaking_supports = BooleanField()
+
+
+class UserSettings(Document):
+    location_sharing_on = BooleanField()
+    location = PointField()
+    preferences = EmbeddedDocumentField(Preferences)  # References Preferences
+
+
+class Profile(Document):
+    first_name = StringField(required=True)
+    last_name = StringField(required=True)
+    date_of_birth = DateTimeField(required=True)
+    gender = StringField(required=True)
+    settings = ReferenceField(UserSettings)  # References Settings
+
+
+class User(Document):
+    user_id = IntField(unique=True, required=True)
+    username = StringField(unique=True, required=True)
+    password = StringField(required=True)  # todo, min length or store a hash
+    email = EmailField(required=True)
+    registered = BooleanField(required=True, default=False)  # *** set the to true when created profile
+    profile = ReferenceField(Profile)  # References Profile
+    date_created = DateField(default=datetime.utcnow)
+
+    meta = {
+        "indexes": ["user_id"],
+        "ordering": ["-date_created"]
+    }
