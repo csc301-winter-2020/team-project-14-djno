f32ac6ceae02cf785cb7b324bf136cbb8d03ddca
asadaleem <asadaleem08@hotmail.com>
2020-02-22 15:58:37 -0500

    suggestion to save google authorization code in user object for future use.

--- backend/model/UserModel.py
+++ backend/model/UserModel.py
@@ -34,6 +34,11 @@ class Profile(EmbeddedDocument):
     settings = EmbeddedDocumentField(UserSettings)
 
 
+# suggest the user authorization code received from google also be stored in the User object,
+# just in case user disputes information in future, and we may need the authorization code
+# to take up the matter with google. Although this aspect is not part of our app at this point
+# but it may be good idea to store this information.
+
 class User(Document):
     user_id = StringField(unique=True, required=True)
     password = StringField(required=True)  # todo, min length or store a hash
