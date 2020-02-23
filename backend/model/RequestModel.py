327848fb3c7ed243722d32e57d41ccd861171e2b
asadaleem <asadaleem08@hotmail.com>
2020-02-23 00:42:42 -0500

    some comments added and preferences included here, and made changes to request document

--- backend/model/RequestModel.py
+++ backend/model/RequestModel.py
@@ -7,6 +7,50 @@ from backend.model.UserModel import Preferences
 # todo: add default values and data length restrictions
 
 
+# It is suggested that the Preferences object be moved to the Request Model, because
+# requests will be based on Preferences.
+
+class Preferences(EmbeddedDocument):
+    education_navigation = BooleanField()
+    education_support = BooleanField()
+    employment_navigation = BooleanField()
+    employment_support = BooleanField()
+    health_care_navigation = BooleanField()
+    health_care_support = BooleanField()
+    local_navigation = BooleanField()
+    local_support = BooleanField()
+    well_being_leisure = BooleanField()
+    pick_up_and_delivery = BooleanField()
+    pick_up_and_drop_off = BooleanField()
+    homemaking_supports = BooleanField()
+
+
+# only those requests to qualify for matching which 1) are not expired 2) are not complete
+# 3) do not have an acceptor.
+
+# to facilitate matching, both assistance_required and assistance_offerred fields are kept
+
+# function required to extend expiry
+# funtion required to delete acceptance. Both, requester and accepto to have delete option
+# no need to have an offer extending & accepting mechanism. I acceptor is not agreeable, 
+# requestor may simply cancel acceptance.
+
+class Request(Document):
+    request_id = IntField(unique=True, required=True)
+    requestor = ReferenceField(
+        User, required=True, reverse_delete_rule=CASCADE)
+    gender = StringField(required=True)
+    age = IntField(required=True)
+    assistance_required = EmbeddedDocumentField(Preferences, required=True)
+    assistance_offerred = EmbeddedDocumentField(Preferences, required=True)
+    description = StringField(required=True, max_length=280)
+    time_created = DateTimeField(required=True, default=datetime.utcnow)
+    expiry_request = DateField(required=True, default=datetime.utcnow)
+    is_completed = BooleanField(required=True, default=False)
+    acceptor = ReferenceField(User)
+
+'''
+
 class Request(Document):
     request_id = IntField(unique=True, required=True)
     requestor = ReferenceField(
@@ -35,3 +79,5 @@ class Request(Document):
 #     acceptor_user = ReferenceField(User, required=True)
 #     time_accepted = DateField(required=True, default=datetime.utcnow)
 #     userId = StringField(required=True)
+
+'''
