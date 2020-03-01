11e1995db6dc8d9ac229d6ffb1508be0688a3798
David Islam <davidanatolie@Davids-MacBook-Pro.local>
2020-03-01 13:30:02 -0500

    update request form

---
+++ deliverable-2/form/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import Form from "./components/Form.js";
+
+class App extends React.Component {
+  state = {
+    fields: {}
+  };
+
+  onChange = updatedValue => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        ...updatedValue
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Make a new request</h1>
+        <Form onChange={fields => this.onChange(fields)} />
+        <p>{JSON.stringify(this.state.fields, null, 2)}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
