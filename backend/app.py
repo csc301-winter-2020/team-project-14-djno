4dfb4e35bbf3cc7404ee25c80542437f2cf4fcc3
aionWilliam <zhaiwilliam@gmail.com>
2020-03-04 15:23:36 -0500

    implemented routes for create profile and user page

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route("create_user_with_gmail", methods=['POST'])
+@app.route("/create_user_with_gmail", methods=['POST'])
 def create_user_with_gmail():
     data = request.get_json()
     user_id = data['user_id']
@@ -29,16 +29,41 @@ def login_verify():
 
     if service.get_user_by_userId(user_id) is False:
         # user does not exist
-        jsonify({"login_success": False})
+        return jsonify({"login_success": False})
     else:
         session['tokenID'] = tokenID
-        jsonify({"login_success": True})
+        return jsonify({"login_success": True})
+
+@app.route("/user/create_profile", methods=['POST'])
+def create_profile():
+    data = request.get_json()
+    user_id = data['user_id']
+    first_name = data['first_name']
+    last_name = data['last_name']
+    date_of_birth = data['date_of_birth']
+    gender = data['gender']
+
+    profile = service.create_profile(user_id, first_name, last_name, date_of_birth, gender)
+    if profile is False:
+        return jsonify({"create_profile_success": False})
+    else:
+        return jsonify({"create_profile_success": True})
+
+@app.route("/user/<int:user_id>", methods=['GET'])
+def user_page(user_id):
+    # front-end should call this function once user have successfully logged in
+    user = service.get_user_by_userId(user_id)
+    if user is False:
+        return jsonify({"profile_exist": False})
+    else:
+        return jsonify({"profile_exist": True, "profile": user.json()})
 
 @app.route("/preference", methods=["POST"])
 def preference_match():
     data = request.get_json()
     current_pref_v = PreferenceVector.build_vector(data)
     return "good"
+
 if __name__ == "__main__":
     res = connect(DATABASE_NAME, host=HOST_IP, port=PORT, username=USERNAME, password=PASSWORD,
               authentication_source=AUTHENTICATION_SOURCE)
