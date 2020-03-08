0e665d5c28fae9860b422c447315c5bc6c20a9b6
Truman Hung <t.hung@mail.utoronto.ca>
2020-03-07 19:50:23 -0500

    Frontend: Profile page is functional

--- backend/app.py
+++ backend/app.py
@@ -121,6 +121,6 @@ def preference_match():
 if __name__ == "__main__":
     res = mongoengine.connect(DATABASE_NAME, host=HOST_IP, port=PORT, username=USERNAME, password=PASSWORD,
                   authentication_source=AUTHENTICATION_SOURCE)
-    print("The server is launchuing....")
+    print("The server is launching....")
     # Session(app)
     app.run(host="0.0.0.0", port=os.environ.get('PORT', 8080))
