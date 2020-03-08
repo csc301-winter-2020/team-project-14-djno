f75a63c778fb69c81b85adbda2a85eb7c39ed2ae
Truman Hung <t.hung@mail.utoronto.ca>
2020-03-08 16:49:48 -0400

    Frontend: Setting posting

--- backend/static/assets/js/home.js
+++ backend/static/assets/js/home.js
@@ -1,52 +1,104 @@
-let profile;
+$(document).ready(function () {
+    // Print localStorage data
+    console.log("Local storage data:\n================================================");
+    const dataKeys = Object.keys(localStorage);
+    for (i in dataKeys) {
+        console.log(`${dataKeys[i]}: ${localStorage.getItem(dataKeys[i])}`);
+    }
+    console.log("================================================");
 
-$ (document).ready (function () {
-  // console.log(document.cookie);
-  let data = localStorage.getItem("data")
-  console.log("my data is: ", data)
-  $.when (login(), get_user_profile (data["email"])).done (() => {
     /* Change DOM content */
     // First name
-    document.querySelectorAll ('#profile-first-name').forEach (first_name => {
-      first_name.innerText = profile.first_name;
+    document.querySelectorAll('#profile-first-name').forEach(first_name => {
+        first_name.innerText = localStorage.getItem("first_name");
     });
 
     /* Listeners */
+    // Update any setting once there is a change.
+    var settingItem = $("select.custom-select.custom-select-sm.d-table.float-right")
+        .map(function () {
+            $(this).change(function () {
+                updateSetting(this);
+            });
+        });
 
     // Home Page
     document
-      .querySelector ('#decline-request')
-      .addEventListener ('click', a => {
-        declineRequest (a.target.offsetParent);
-      });
+        .querySelector('#decline-request')
+        .addEventListener('click', a => {
+            declineRequest(a.target.offsetParent);
+        });
 
-    document.querySelector ('#chat-request').addEventListener ('click', a => {
-      chat (a.target.offsetParent);
+    document.querySelector('#chat-request').addEventListener('click', a => {
+        chat(a.target.offsetParent);
     });
-  });
 });
 
 /* Functions */
 
-function declineRequest (profile) {
-  profile.remove ();
-  console.log ('TODO: send declineRequest to server');
+function declineRequest(profile) {
+    profile.remove();
+    console.log('TODO: send declineRequest to server');
 }
 
-function chat (profile) {
-  console.log ('TODO: chat with profile user');
+function chat(profile) {
+    console.log('TODO: chat with profile user');
 }
 
+function updateSetting(selectObj) {
+    const key = selectObj.name;
+    const returnObj = {};
+    returnObj["email"] = localStorage.getItem("email");
+
+    // Currently we only allow update a pair of key
+    returnObj[key] = $(selectObj).val();
+
+    console.log(`Updating ${key} to ${returnObj[key]}`);
+
+
+    return $.ajax({
+        type: 'POST',
+        url: '/user/settings',
+
+        // The key needs to match your method's input parameter (case-sensitive).
+        data: JSON.stringify(returnObj),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: data => {
+            console.log(`Update setting: ${data.update_settings_success}`);
+        },
+        failure: function (errMsg) {
+            console.log(`Update setting failed: ${errMsg}`);
+        },
+    });
+}
+
+function selectAllOptions(obj, bool) {
+    let selectObj = obj.closest(".form-group").children[2];
+    console.log(selectObj);
+
+    // Front-end side
+    if (bool) {
+        for (let i = 0; i < selectObj.options.length; i++) {
+            selectObj.options[i].selected = true;
+        }
+    } else {
+        for (let i = 0; i < selectObj.options.length; i++) {
+            selectObj.options[i].selected = false;
+        }
+    }
+
+    // Back-end side
+    updateSetting(selectObj);
+}
 
 // Retrieve User Profile
 function get_user_profile (email) {
-  console.log("I'm sending..." + email)
-  return $.get (`/user/email/${email}`, function (
+  return $.get (`/user/settings/${email}`, function (
     data,
     status
   ) {
     console.log (`Retrieve user profile: ${status}`);
-    profile = data.profile;
     console.log (profile);
   });
 }
--- backend/static/assets/js/profile.js
+++ backend/static/assets/js/profile.js
@@ -1,151 +1,185 @@
 var profile;
 
-$ (document).ready (function () {
-  $.when (login (), get_user_profile ('johnsmith010@gmail.com')).done (() => {
+$(document).ready(function () {
     /* Change DOM content */
-    document.querySelector ('input[name="first-name"]').value =
-      profile.first_name;
-    document.querySelector ('input[name="last-name"]').value =
-      profile.last_name;
-    document.querySelector ('input[name="email"]').value = profile.email;
+    document.querySelector('input[name="first-name"]').value =
+        profile.first_name;
+    document.querySelector('input[name="last-name"]').value =
+        profile.last_name;
+    document.querySelector('input[name="email"]').value = localStorage.getItem("email");
 
     // DOB
-    const d = new Date (profile.date_of_birth);
-    const ye = new Intl.DateTimeFormat ('en', {year: 'numeric'}).format (d);
-    const mo = new Intl.DateTimeFormat ('en', {month: '2-digit'}).format (d);
-    const da = d.getUTCDate ();
-    document.querySelector ('input[name="dob"]').value = `${ye}-${mo}-${da}`;
+    const d = new Date(profile.date_of_birth);
+    const ye = new Intl.DateTimeFormat('en', {year: 'numeric'}).format(d);
+    const mo = new Intl.DateTimeFormat('en', {month: '2-digit'}).format(d);
+    const da = d.getUTCDate();
+    document.querySelector('input[name="dob"]').value = `${ye}-${mo}-${da}`;
 
     // Gender
-    let optionExist = document.querySelector (
-      `select[name="gender"] option[value="${profile.gender}"]`
+    let optionExist = document.querySelector(
+        `select[name="gender"] option[value="${profile.gender}"]`
     );
     if (optionExist) {
-      document.querySelector ('select[name="gender"]').value = profile.gender;
+        document.querySelector('select[name="gender"]').value = profile.gender;
     } else {
-      document.querySelector ('select[name="gender"]').value = 'null';
+        document.querySelector('select[name="gender"]').value = 'null';
     }
 
     /* Listeners */
+    // Update any setting once there is a change.
+    var settingItem = $("select.custom-select.custom-select-sm.d-table.float-right")
+        .map(function () {
+            $(this).change(function () {
+                const key = this.name;
+                const val = $(this).val();
+                updateSetting(key, val);
+            });
+        });
 
     // Edit profile button
-    const editProfileBtn = document.querySelector ('#edit-profile');
+    const editProfileBtn = document.querySelector('#edit-profile');
 
-    editProfileBtn.addEventListener ('click', a => {
-      // When it is an Edit button.
-      if (editProfileBtn.classList.contains ('btn-primary')) {
-        // Button
-        editProfileBtn.classList.add ('btn-info');
-        editProfileBtn.classList.remove ('btn-primary');
-        editProfileBtn.innerText = 'Save';
+    editProfileBtn.addEventListener('click', a => {
+        // When it is an Edit button.
+        if (editProfileBtn.classList.contains('btn-primary')) {
+            // Button
+            editProfileBtn.classList.add('btn-info');
+            editProfileBtn.classList.remove('btn-primary');
+            editProfileBtn.innerText = 'Save';
 
-        // Enable fields
-        document.querySelectorAll ('input, select').forEach (field => {
-          if (field.name != 'email') {
-            field.disabled = false;
-          }
-        });
-      } else {
-        // When it is a Save button.
-        // Check if fields values are valid.
-        let validSave = true;
-        document.querySelectorAll ('input, select').forEach (field => {
-          // Reset invalid style
-          field.classList.remove ('invalid');
-
-          if (field.value.length == 0) {
-            validSave = false;
-            field.classList.add ('invalid');
-          }
-        });
-
-        if (validSave) {
-          // Button
-          editProfileBtn.classList.add ('btn-primary');
-          editProfileBtn.classList.remove ('btn-info');
-          editProfileBtn.innerText = 'Edit';
-
-          // Disable fields
-          document.querySelectorAll ('input, select').forEach (field => {
-            field.disabled = true;
-          });
-
-          // Send to server
-          let firstName = document.querySelector ('input[name="first-name"]')
-            .value;
-          let lastName = document.querySelector ('input[name="last-name"]')
-            .value;
-          let DOB = document.querySelector ('input[name="dob"]').value;
-          let gender = document.querySelector ('select[name="gender"]').value;
-          let email = document.querySelector ('input[name="email"]').value;
-
-          saveProfile (firstName, lastName, DOB, gender, email);
+            // Enable fields
+            document.querySelectorAll('input, select').forEach(field => {
+                if (field.name != 'email') {
+                    field.disabled = false;
+                }
+            });
         } else {
-          console.log ('Invalid');
+            // When it is a Save button.
+            // Check if fields values are valid.
+            let validSave = true;
+            document.querySelectorAll('input, select').forEach(field => {
+                // Reset invalid style
+                field.classList.remove('invalid');
+
+                if (field.value.length == 0) {
+                    validSave = false;
+                    field.classList.add('invalid');
+                }
+            });
+
+            if (validSave) {
+                // Button
+                editProfileBtn.classList.add('btn-primary');
+                editProfileBtn.classList.remove('btn-info');
+                editProfileBtn.innerText = 'Edit';
+
+                // Disable fields
+                document.querySelectorAll('input, select').forEach(field => {
+                    field.disabled = true;
+                });
+
+                // Send to server
+                let firstName = document.querySelector('input[name="first-name"]')
+                    .value;
+                let lastName = document.querySelector('input[name="last-name"]')
+                    .value;
+                let DOB = document.querySelector('input[name="dob"]').value;
+                let gender = document.querySelector('select[name="gender"]').value;
+                let email = document.querySelector('input[name="email"]').value;
+
+                saveProfile(firstName, lastName, DOB, gender, email);
+            } else {
+                console.log('Invalid');
+            }
         }
-      }
     });
-  });
 });
 
 /* Functions */
-function chat (profile) {
-  console.log ('TODO: chat with profile user');
+function chat(profile) {
+    console.log('TODO: chat with profile user');
 }
 
 // AJAX Login
-function login () {
-  return $.ajax ({
-    type: 'POST',
-    url: '/login',
-    // The key needs to match your method's input parameter (case-sensitive).
-    data: JSON.stringify ({
-      token_id: 'asd',
-      email: 'at@te.com',
-    }),
-    contentType: 'application/json; charset=utf-8',
-    dataType: 'json',
-    success: data => {
-      console.log (`Login: ${data.login_success}`);
-    },
-    failure: function (errMsg) {
-      console.log (`Login failed: ${errMsg}`);
-    },
-  });
-}
+// function login () {
+//   return $.ajax ({
+//     type: 'POST',
+//     url: '/login',
+//     // The key needs to match your method's input parameter (case-sensitive).
+//     data: JSON.stringify ({
+//       token_id: 'asd',
+//       email: 'at@te.com',
+//     }),
+//     contentType: 'application/json; charset=utf-8',
+//     dataType: 'json',
+//     success: data => {
+//       console.log (`Login: ${data.login_success}`);
+//     },
+//     failure: function (errMsg) {
+//       console.log (`Login failed: ${errMsg}`);
+//     },
+//   });
+// }
 
-// Retrieve User Profile
-function get_user_profile (email) {
-  return $.get (`/user/email/${email}`, function (
-    data,
-    status
-  ) {
-    console.log (`Retrieve user profile: ${status}`);
-    profile = data.profile;
-    console.log (profile);
-  });
-}
+// // Retrieve User Profile
+// function get_user_profile (email) {
+//   return $.get (`/user/email/${email}`, function (
+//     data,
+//     status
+//   ) {
+//     console.log (`Retrieve user profile: ${status}`);
+//     profile = data.profile;
+//     console.log (profile);
+//   });
+// }
 
 // Save Profile
-function saveProfile (firstName, lastName, DOB, gender, email) {
-  return $.ajax ({
-    type: 'POST',
-    url: '/user/profile',
-    // The key needs to match your method's input parameter (case-sensitive).
-    data: JSON.stringify ({
-      first_name: firstName,
-      last_name: lastName,
-      date_of_birth: DOB,
-      gender: gender,
-      email: email,
-    }),
-    contentType: 'application/json; charset=utf-8',
-    dataType: 'json',
-    success: data => {
-      console.log (`Create profile: ${data.create_profile_success}`);
-    },
-    failure: function (errMsg) {
-      console.log (`Create profile failed: ${errMsg}`);
-    },
-  });
+function saveProfile(firstName, lastName, DOB, gender, email) {
+    return $.ajax({
+        type: 'POST',
+        url: '/user/profile',
+        // The key needs to match your method's input parameter (case-sensitive).
+        data: JSON.stringify({
+            first_name: firstName,
+            last_name: lastName,
+            date_of_birth: DOB,
+            gender: gender,
+            email: email,
+        }),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: data => {
+            console.log(`Create profile: ${data.create_profile_success}`);
+        },
+        failure: function (errMsg) {
+            console.log(`Create profile failed: ${errMsg}`);
+        },
+    });
+}
+
+
+function updateSetting(name, value) {
+    const key = name;
+    const returnObj = {};
+    returnObj["email"] = profile.email;
+
+    // Currently we only allow update a pair of key
+    returnObj[key] = value;
+
+
+    return $.ajax({
+        type: 'POST',
+        url: '/user/settings',
+
+        // The key needs to match your method's input parameter (case-sensitive).
+        data: JSON.stringify(returnObj),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: data => {
+            console.log(`Update setting: ${data.login_success}`);
+        },
+        failure: function (errMsg) {
+            console.log(`Update setting failed: ${errMsg}`);
+        },
+    });
 }
