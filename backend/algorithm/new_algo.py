379872bde3ee7266fb336f4b465d7b5cf743c93e
David Islam <davidanatolie@Davids-MacBook-Pro.local>
2020-03-13 19:15:34 -0400

    Worked on filtering system

--- backend/algorithm/new_algo.py
+++ backend/algorithm/new_algo.py
@@ -1,8 +1,9 @@
 from config import *
 from UserModel import *
 
+""" A filtering system """
 
-candidates = User.objects()
+candidates = Profile.objects()
 
 
 def get_matches(data):
@@ -19,4 +20,19 @@ def get_matches(data):
         "description" : <description of request>
     }
     """
+    coresp_pref = service_to_pref[data["category"]]
+    UserSettings.objects(preferences=coresp_pref)
     return candidates
+
+
+""" Pseudocode for Step 2
+
+"""
+
+""" Step 3
+
+For time HH:MM AM/PM, get the HH part and check
+
+    Convert given time to military time
+
+"""
