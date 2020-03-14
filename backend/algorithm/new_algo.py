5756ab291f73589fa93ae6942e2ab53b58516e4d
David Islam <davidanatolie@Davids-MacBook-Pro.local>
2020-03-14 17:22:36 -0400

    Update new_algo.py

--- backend/algorithm/new_algo.py
+++ backend/algorithm/new_algo.py
@@ -1,38 +1,58 @@
+from datetime import datetime
+# from mongoengine import *
 from config import *
-from UserModel import *
+from backend.model.UserModel import *
 
 """ A filtering system """
 
-candidates = Profile.objects()
+# candidates = Profile.objects
 
 
 def get_matches(data):
     """
-    Returns a list of candidate objects.
+    Returns a list of profile objects.
 
     data: JSON data of the form
         {
         "requestor_email" : <requestor's email>,
         "title" : <name of request>,
         "location" : (latitude, longitude),
-        "datetime" : YYYY-MM-DD, HH:SS AM/PM,
+        "datetime" : <datetime object>,
         "category" : <codename of category>,
         "description" : <description of request>
     }
+
+    Output: [Profile]
     """
-    coresp_pref = service_to_pref[data["category"]]
-    UserSettings.objects(preferences=coresp_pref)
-    return candidates
+    corresp_pref = service_to_pref[data["category"]]
+    day = data["datetime"].strftime('%A')
+    time = data["datetime"].time
+    time_of_day = get_time_of_day(time.hour)
+
+    qSet1 = UserSettings.objects.get_matched_pref(corresp_pref)
+    qSet2 = UserSettings.objects.get_matched_time(day, time_of_day)
+    qSet3 = UserSettings.objects.get_gps(False)
+    qSet4 = User.objects.get_nearby(data["location"])
+
+    # Now I want to join qSets with Profile by "email". Use Aggregate function
+    # with '$lookup'
+
+    pipeline = []
 
 
-""" Pseudocode for Step 2
+def get_time_of_day(hour):
+    if 0 <= hour <= 5:
+        return "Night"
+    if 6 <= hour <= 11:
+        return "Morning"
+    if 12 <= hour <= 17:
+        return "Afternoon"
+    if 18 <= hour <= 23:
+        return "Evening"
+    else:
+        return "hour is invalid"
 
-"""
 
-""" Step 3
-
-For time HH:MM AM/PM, get the HH part and check
-
-    Convert given time to military time
-
-"""
+if __name__ == "__main__":
+    connect(DATABASE_NAME, HOST_IP, PORT,
+            AUTHENTICATION_SOURCE, USERNAME, PASSWORD)
