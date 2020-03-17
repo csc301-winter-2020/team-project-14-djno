49ab7a1780e4e18fd8b7849d9d26bf5b1df61092
David Islam <davidanatolie@Davids-MacBook-Pro.local>
2020-03-17 15:38:26 -0400

    Update new_algo

--- backend/algorithm/new_algo.py
+++ backend/algorithm/new_algo.py
@@ -3,9 +3,23 @@ from datetime import datetime
 from config import *
 from backend.model.UserModel import *
 
-""" A filtering system """
+""" A filtering system 
 
-# candidates = Profile.objects
+Example usage:
+
+Say requester seeks helpers who are available on a Saturday afternoon for phone call
+qSet1 = Settings.objects.filter_by_pref("OPC")
+qSet2 = Settings.objects.filter_by_day("Saturday")
+qSet3 = Settings.objects.filter_by_time("Afternoon")
+qSet4 = User.objects.filter_by_location([1,1])
+
+Now find users who satisfy all criteria
+Step 1: For each qSet, get list of emails
+Step 2: Convert each list into a set
+Step 3: Find intersection of all sets
+
+Then get corresp profile object
+"""
 
 
 def get_matches(data):
@@ -16,31 +30,33 @@ def get_matches(data):
         {
         "requestor_email" : <requestor's email>,
         "title" : <name of request>,
-        "location" : (latitude, longitude),
+        "location" : [latitude, longitude],
         "datetime" : <datetime object>,
         "category" : <codename of category>,
         "description" : <description of request>
     }
 
-    Output: [Profile]
+    Output: [Profile], False otherwise
     """
     corresp_pref = service_to_pref[data["category"]]
     day = data["datetime"].strftime('%A')
     time = data["datetime"].time
     time_of_day = get_time_of_day(time.hour)
 
-    qSet1 = UserSettings.objects.get_matched_pref(corresp_pref)
-    qSet2 = UserSettings.objects.get_matched_time(day, time_of_day)
-    qSet3 = UserSettings.objects.get_gps(False)
-    qSet4 = User.objects.get_nearby(data["location"])
+    if not time_of_day:
+        return False
+    if day not in days:
+        return False
 
-    # Now I want to join qSets with Profile by "email". Use Aggregate function
-    # with '$lookup'
+    qSet1 = Settings.objects.filter_by_pref(corresp_pref)
+    qSet2 = Settings.objects.filter_by_time(time_of_day)
+    qSet3 = Settings.objects.filter_by_day(day)
+    qSet4 = User.objects.filter_by_location(data["location"])
 
-    pipeline = []
+    # TODO
 
 
-def get_time_of_day(hour):
+def get_time_of_day(hour) -> str:
     if 0 <= hour <= 5:
         return "Night"
     if 6 <= hour <= 11:
@@ -50,9 +66,9 @@ def get_time_of_day(hour):
     if 18 <= hour <= 23:
         return "Evening"
     else:
-        return "hour is invalid"
+        return False
 
 
 if __name__ == "__main__":
-    connect(DATABASE_NAME, HOST_IP, PORT,
+    connect("david", HOST_IP, PORT,
             AUTHENTICATION_SOURCE, USERNAME, PASSWORD)
