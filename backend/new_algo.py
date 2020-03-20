28c3898c34908c2201e54759338f145a5133073a
David Islam <davidanatolie@Davids-MacBook-Pro.local>
2020-03-19 23:28:54 -0400

    Making new_algo more efficient

--- backend/new_algo.py
+++ backend/new_algo.py
@@ -4,7 +4,7 @@ from config import *
 from model.UserModel import User, Settings
 from service.UserService import get_user_profile_by_email
 
-""" A filtering system 
+""" A filtering system
 """
 
 
@@ -44,24 +44,29 @@ def get_matches(data, pref=True, day=True, time=True, loc=True):
     if not time_of_day or day not in days or corresp_pref not in p_rules:
         return False
 
-    # Perform filters
-    qSet1 = Settings.objects.filter_by_pref(corresp_pref)
-    qSet2 = Settings.objects.filter_by_time(time_of_day)
-    qSet3 = Settings.objects.filter_by_day(day)
-    qSet4 = User.objects.filter_by_location(data["location"])
+    qSet = Settings.objects(Q(corresp_pref=True) & Q(
+        day=True) & Q(time_of_day=True)).only('email')
 
-    # For each qSet, project out emails
-    set1 = set([s1.email for s1 in qSet1])
-    set2 = set([s2.email for s2 in qSet2])
-    set3 = set([s3.email for s3 in qSet3])
-    set4 = set([s4.email for s4 in qSet4])
+    # # Perform filters
+    # qSet1 = Settings.objects.filter_by_pref(corresp_pref)
+    # qSet2 = Settings.objects.filter_by_time(time_of_day)
+    # qSet3 = Settings.objects.filter_by_day(day)
+    # qSet4 = User.objects.filter_by_location(data["location"])
 
-    # Join above sets i.e. compute the intersection
-    final_set = set1.intersection(set2).intersection(set3).intersection(set4)
+    # # For each qSet, project out emails
+    # set1 = set([s1.email for s1 in qSet1])
+    # set2 = set([s2.email for s2 in qSet2])
+    # set3 = set([s3.email for s3 in qSet3])
+    # set4 = set([s4.email for s4 in qSet4])
 
-    # Now get the corresponding profile objects
-    rs = [get_user_profile_by_email(e) for e in final_set]
+    # # Join above sets i.e. compute the intersection
+    # final_set = set1.intersection(set2).intersection(set3).intersection(set4)
 
+    # # Now get the corresponding profile objects
+    # rs = [get_user_profile_by_email(e) for e in final_set]
+    rs = [get_user_profile_by_email(e) for e in qSet]
+    # TODO: settings.x, x is a variable. How do I make it work?
+    # return User.filter_by_location(data["location"]).filter(Q(settings.corresp_pref=True) & Q(settings.day=True) & Q(settings.time_of_day=True)).only('profile')
     return rs
 
 
