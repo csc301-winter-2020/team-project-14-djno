ed00b7c48d45db624be23de063411be2559fc6fd
weiyihu <huweiyi1995@gmail.com>
2020-03-25 03:29:28 -0400

    UI

--- backend/static/assets/js/chat_function.js
+++ backend/static/assets/js/chat_function.js
@@ -123,13 +123,15 @@ $(document).ready(function() {
 `;
   $("#contact-list").append(sample_user);
   $("#contact-list").append(newUser());
+  receiveMessage({ message: "haha", src: "ihosdsd" });
+
   let is_connected = false;
   var socket = io();
   socket.on("chat", function(msg) {
     console.log("message is....");
     console.log(msg);
-    // //reveice message
-    // receiveMessage(msg)
+    //reveice message
+    receiveMessage(msg);
   });
   socket.on("connect", function() {
     socket.emit("join", {
@@ -216,6 +218,26 @@ function sendMessage() {
     return ReMessage;
   }
 }
+
+function receiveMessage(msg) {
+  let from = msg["src"];
+  let message = msg["message"];
+
+  let reMessage = `
+                <div class="media w-50 mb-3"><img
+                src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50"
+                class="rounded-circle">
+              <div class="media-body ml-3">
+                <div class="bg-light rounded py-2 px-3 mb-2">
+                  <p class="text-small mb-0 text-muted">${message}</p>
+                </div>
+                <p class="small text-muted">${date()}</p>
+              </div>
+              </div> 
+  `;
+  $("#chat-box").append(reMessage);
+}
+
 // function loadPreviousMessage() {
 
 // }
