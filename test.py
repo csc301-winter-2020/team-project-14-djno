d6fc0dea109cd438580de1aa0df3b84757b11ee6
asadaleem <59983555+asadaleem@users.noreply.github.com>
2020-03-27 18:59:04 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+from backend.test.service.user_service_test import TestUserService
+from backend.test.service.request_service_test import TestRequestService
+from backend.test.algorithm.new_algo_test1 import TestNewAlgo
+import unittest
+
+import unittest
+
+class testService(TestUserService):
+
+    pass
+
+class testService(TestRequestService):
+
+    pass
+
+class testService(TestNewAlgo):
+
+    pass 
+
+
+if __name__ == "__main__":
+    unittest.main()
